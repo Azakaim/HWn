c2d6fd9ec458f31bb86b9312f3925c1c9c7d0623
Azakaim <91306580+Azakaim@users.noreply.github.com>
2021-09-27 12:38:38 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создание базы данных из 20 сотрудников
+            Repository repository = new Repository(20);
+
+            // Печать в консоль всех сотрудников
+            repository.Print("База данных до преобразования");
+
+            Console.ReadKey();
+        }
+    }
+}
